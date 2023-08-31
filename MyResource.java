d54c637d2eff4583ec8897bacad51b7114003253
Ahtsham Sohail <96394328+Sham1242@users.noreply.github.com>
2023-08-31 17:10:20 -0400

    Update MyResource.java

--- MyResource.java
+++ MyResource.java
@@ -1,8 +1,8 @@
-import org.apache.coyote.Response;
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+package TaskMaster.Jersey;
+
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import java.io.*;
 import java.util.*;
 
@@ -19,22 +19,9 @@ public class MyResource {
     public List<Task> getTasks() {
         List<Task> tasks = new ArrayList<>();
 
-        try (FileInputStream fis = new FileInputStream(EXCEL_FILE_PATH);
-             Workbook workbook = new XSSFWorkbook(fis)) {
-
-            Sheet sheet = workbook.getSheetAt(0);
-
-            for (Row row : sheet) {
-                if (row.getRowNum() == 0) {
-                    continue; // Skip the header row
-                }
-
-                String name = row.getCell(0).getStringCellValue();
-                String date = row.getCell(1).getStringCellValue();
-                String progress = row.getCell(2).getStringCellValue();
-
-                tasks.add(new Task(name, date, progress));
-            }
+        try {
+            List<Task> excelTasks = readExcelData(EXCEL_FILE_PATH);
+            tasks.addAll(excelTasks);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -43,43 +30,92 @@ public class MyResource {
 
         return tasks;
     }
+
     @GET
     @Path("edit")
     public Response editTask(@QueryParam("task") String taskName,
                              @QueryParam("newDate") String newDate,
                              @QueryParam("newProgress") String newProgress) {
-        try (FileInputStream fis = new FileInputStream(EXCEL_FILE_PATH);
-             Workbook workbook = new XSSFWorkbook(fis);
-             FileOutputStream fos = new FileOutputStream(EXCEL_FILE_PATH)) {
+        try {
+            List<Task> excelTasks = readExcelData(EXCEL_FILE_PATH);
 
-            Sheet sheet = workbook.getSheetAt(0);
-
-            for (Row row : sheet) {
-                if (row.getRowNum() == 0) {
-                    continue; // Skip the header row
-                }
-
-                Cell cell = row.getCell(0);
-                if (cell != null && cell.getStringCellValue().equals(taskName)) {
-                    // Update the date and progress
-                    Cell dateCell = row.getCell(1);
-                    Cell progressCell = row.getCell(2);
-                    
-                    dateCell.setCellValue(newDate);
-                    progressCell.setCellValue(newProgress);
-                    break; // Task found and updated, no need to continue
+            for (Task task : excelTasks) {
+                if (task.getName().equals(taskName)) {
+                    task.setDate(newDate);
+                    task.setProgress(newProgress);
+                    saveExcelData(excelTasks);
+                    return Response.ok("Task updated: " + taskName).build();
                 }
             }
-
-            // Save the changes to the Excel file
-            workbook.write(fos);
-
-            return Response.ok("Task updated: " + taskName).build();
 
         } catch (IOException e) {
             e.printStackTrace();
             // Handle the exception as needed
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
-}
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    // Utility methods for reading and writing Excel data
+    private List<Task> readExcelData(String filePath) throws IOException {
+        List<Task> tasks = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 3) {
+                    String name = parts[0];
+                    String date = parts[1];
+                    String progress = parts[2];
+                    Task task = new Task(name, date, progress);
+                    tasks.add(task);
+                   
+                }
+            }
+        }
+        return tasks;
+    }
+
+    private void saveExcelData(List<Task> tasks) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(EXCEL_FILE_PATH))) {
+            for (Task task : tasks) {
+                writer.write(task.getName() + "," + task.getDate() + "," + task.getProgress());
+                writer.newLine();
+            }
+        }
+    }
+
+    // Task class
+    private static class Task {
+        private String name;
+        private String date;
+        private String progress;
+
+        public Task(String name, String date, String progress) {
+            this.name = name;
+            this.date = date;
+            this.progress = progress;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public String getProgress() {
+            return progress;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public void setProgress(String progress) {
+            this.progress = progress;
+        }
+    }
 }
